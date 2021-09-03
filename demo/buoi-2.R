6d6269d8ee70ec29d2f3ad606303e7bb5625dfab
pham-k <pham.k@protonmail.com>
2021-09-03 18:53:32 +0700

    Add dataset

---
+++ demo/buoi-2.R
@@ -0,0 +1,60 @@
+# Khai bao function
+add <- function(a, b) {
+  a + b
+  2 * a
+}
+
+# Goi function
+add(2, 3)
+
+# Control statement
+# if else
+hello <- function(s) {
+  if (length(s) > 5) {
+    print(a)
+  } else {
+    print('aaaaa')
+  }
+}
+
+hello('Khuong')
+
+# for
+for (i in c(1:3)) {
+  print(i)
+}
+
+# while
+k <- 5
+while (k > 0) {
+  print(k)
+  k <- k - 1
+  next
+}
+
+# quan huyen
+# ma hoa: noi thanh hoac ngoai thanh
+mahoa(qh) {
+  if qh trong ds noi thanh {
+    return noi thanh
+  } else {
+    return khac
+  }
+}
+
+for(n in c(2,50,100,200,500)) {
+  x <- stats::rnorm(n)
+  cat(n, ": ", mean(x), '\n', sep = "_")
+}
+
+library(sdasd)
+
+
+
+
+
+
+
+
+
+
